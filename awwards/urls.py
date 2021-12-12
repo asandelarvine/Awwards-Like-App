e7b8eab30d2f74f76238fd4d2ff9c34d2c1e46fa
asandelarvine <asandelarvine@gmail.com>
2021-12-12 20:52:51 +0300

    added a url for new_project and the users profile

--- awwards/urls.py
+++ awwards/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^$', views.index, name= 'index'),
     url(r'^search/', views.search_projects, name='search_results'),
     url(r'^project/(\d+)', views.get_project, name='project_results'),
+    url(r'^new/project$', views.new_project, name='new-project'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
     
 ]
 
