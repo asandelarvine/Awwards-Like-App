65107f742d0e847caf2af21a276194dd8b2201d9
asandelarvine <asandelarvine@gmail.com>
2021-12-12 21:05:31 +0300

    set a ProfileUpdateForm class and imported registrationform

--- awwards/forms.py
+++ awwards/forms.py
@@ -15,3 +15,12 @@ class NewProjectForm(forms.ModelForm):
         }
         
 
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
+        
+        
