64a2c4a4ab8a6fdb335c2a6e55f0e5e8f32c4ed7
asandelarvine <asandelarvine@gmail.com>
2021-12-12 20:26:28 +0300

    virtual environment

---
+++ env_awwards/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/access/Documents/Django/Django_Projects/Reviews/env_awwards/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
