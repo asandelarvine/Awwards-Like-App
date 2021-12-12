20dce0e9501da6ed007c8163db7143a5cf88a132
asandelarvine <asandelarvine@gmail.com>
2021-12-12 20:40:20 +0300

    created Project class

--- awwards/models.py
+++ awwards/models.py
@@ -36,3 +36,54 @@ class Profile(models.Model):
         verbose_name_plural = 'Profiles'
         
         
+class Projects(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_image = models.ImageField(upload_to = 'images/', default='images/default.jpg')
+    project_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    link = models.URLField()
+    country = CountryField(blank_label='(select country)', default='KE')
+
+        
+    def save_project(self):
+        self.save()
+    
+    def delete_project(self):
+        self.delete()
+        
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(project_title__icontains=search_term)
+        return projects
+    
+    
+    @classmethod
+    def get_by_author(cls, Author):
+        projects = cls.objects.filter(Author=Author)
+        return projects
+    
+    
+    @classmethod
+    def get_project(request, id):
+        try:
+            project = Projects.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return project
+    
+    def __str__(self):
+        return self.project_title
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Project'
+        verbose_name_plural = 'Projects'
